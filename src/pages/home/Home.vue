174f46c265a0cad8ab6e0a3c36d1d232b2dc6a97
kevinzcf <kevinzcf@gmail.com>
2019-11-03 22:21:09 +0100

    project refactoring add compnent home user and posts

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+	<user></user>
+	<posts></posts>
+  </div>
+</template>
+
+<script>
+import User from './components/User'
+import Posts from './components/Posts'
+export default {
+  name: 'Home',
+  components: {
+	User,
+	Posts
+  }
+}
+</script>
