163653ddabb1f0c07e8e33659dc188bc6a0d6520
kevinzcf <kevinzcf@gmail.com>
2019-11-04 00:16:36 +0100

    fetch posts data

--- src/pages/home/components/Posts.vue
+++ src/pages/home/components/Posts.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="posts">
-	posts
+	<div class="post" v-for="item of posts" :key="item.id">
+		<div class="title">{{item.title}}</div>
+		<div class="body">{{item.body}}</div>
+	</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Posts'
+	name: 'posts',
+	data () {
+		return {
+			posts:[]
+		}
+	},
+	mounted(){
+		this.getPosts();
+	},
+	methods:{
+		getPosts(){
+			var url = "http://jsonplaceholder.typicode.com/posts?userId=1";
+			axios.get(url).then((response)=>{
+				this.posts=response.data;
+			});
+			this.posts = "loading...."
+		}
+	}
 }
 </script>
+
+<style lang="stylus" scoped>
+	.post
+		text-align: center
+		padding: 2rem
+		.title	
+			color: #999
+			font-style: italic
+			font-size: 20px
+		.body
+			margin-top: 0.5rem
+</style>
